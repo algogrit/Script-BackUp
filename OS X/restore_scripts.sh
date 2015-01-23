8e244aa76e55bfbfb1847607c96dc564364780d5
Gaurav Agarwal <gauravagarwalr@gmail.com>
2015-01-23 11:03:04 -0800

    Updated Scripts in Darwin

--- OS X/restore_scripts.sh
+++ OS X/restore_scripts.sh
@@ -10,6 +10,8 @@ git stash
 
 echo "\033[1;31mStarting the restore process...\033[0m"
 
+echo "\033[1;31mCreating directories...\033[0m"
+mkdir -p ~/bin ~/.go ~/Custom-Git-Commands
 
 echo "\033[1;31mTapping brews...\033[0m"
 cat ~/Script-BackUp/OS\ X/brew_taps.list | xargs brew tap
@@ -55,16 +57,15 @@ cp ~/Script-BackUp/OS\ X/.powconfig ~/
 echo "\033[1;31mInstall vundle packages...\033[0m"
 vim +BundleInstall +qall
 
-echo "\033[1;31mCreating directory for GOPATH...\033[0m"
-mkdir ~/.go
-
 echo "\033[1;31mRestoring Custom git commands...\033[0m"
-mkdir -p ~/Custom-Git-Commands
 cp ~/Script-BackUp/OS\ X/Custom-Git-Commands/* ~/Custom-Git-Commands/
 
 echo "\033[1;31mSetting Up Sublime...\033[0m"
 cp Sublime/packages.list ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 cp Sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
+echo "\033[1;31mSetup Airport Utility...\033[0m"
+ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport ~/bin
 
 echo "\033[1;31mPopping git stash after restoring...\033[0m"
 git stash pop
