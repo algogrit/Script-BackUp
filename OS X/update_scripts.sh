30619404d3f81a162860810c06e6e6cf30bca653
Gaurav Agarwal <gauravagarwalr@gmail.com>
2015-02-03 10:31:55 -0800

    Updated Scripts in Darwin

--- OS X/update_scripts.sh
+++ OS X/update_scripts.sh
@@ -97,6 +97,9 @@ cp /etc/hosts root/etc/
 cp /etc/irbrc root/etc/
 cp /etc/shells root/etc/
 
+# ENV config
+env > env.config
+
 echo "\033[1;31mRemoving bash unnecessary files...\033[0m"
 rm .bash_history
 
