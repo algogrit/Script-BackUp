ea46d783aef6055139d95195b874471e2d85cf00
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-05-08 11:50:02 +0530

    Updated Scripts in Darwin on wooden-horse.local

--- OS X/mini_update.sh
+++ OS X/mini_update.sh
@@ -6,3 +6,11 @@ brew cleanup
 
 echo "\033[1;31mOutdated brews...\033[0m"
 brew outdated
+
+echo "\033[1;31mUpdating vundle with git...\033[0m"
+cd ~/.vim/bundle/vundle && git ru > /dev/null && git st && git up
+
+echo "\033[1;31mUpdating flutter with git...\033[0m"
+cd ~/Developer/experimental/sdk/flutter && git ru > /dev/null && git st && git up
+flutter --version > ~/Script-BackUp/OS\ X/flutter.version
+flutter doctor -v > ~/Script-BackUp/OS\ X/flutter.info
