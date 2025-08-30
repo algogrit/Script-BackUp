da750ec7e010c69a9a753b1401d6647c55e01a4a
Gaurav Agarwal <algogrit@gmail.com>
2025-08-30 12:19:28 +0530

    Updated Scripts in Darwin on Dhruvtara.local

--- macOS/mini_update.sh
+++ macOS/mini_update.sh
@@ -14,3 +14,7 @@ brew outdated
 
 echo "\033[1;31mUpdating vundle with git...\033[0m"
 cd ~/.vim/bundle/vundle && git ru > /dev/null && git st && git up
+
+echo "\033[1;31m/etc/hosts...\033[0m"
+echo "Current: `cat /etc/hosts | grep -m 1 'Date:' | awk '{print $3 " " $4 " " $5}'`"
+echo "Latest: `curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts > /tmp/hosts.txt 2> /dev/null ; cat /tmp/hosts.txt | grep 'Date:' | awk '{print $3 " " $4 " " $5}'`"
